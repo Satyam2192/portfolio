e9e9af30374e9cf699cd4ca1f404e1612985bb78
Satyam2192 <Satyam21092@gmail.com>
2023-04-01 00:05:33 +0530

    updated home page

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import {
 //   scrollbar-width: none;
 //   &::-webkit-scrollbar{
 //     display: none;
+// bg-[#663399]
 //   }
 // `;
 // =====
@@ -32,7 +33,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="relative z-o bg-primary snap-y snap-mandatory scroll-smooth overflow-auto scrollbar-hide snap-center">
-        <div className="h-[100vh] bg-[#663399] h-100 bg-cover bg-no-repeat bg-center">
+        <div className="h-[100vh] bg-[#339941] h-100 bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
