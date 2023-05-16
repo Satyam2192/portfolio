0a2bf12c5bc68cf053690c5e054b2d087612879e
Satyam2192 <Satyam21092@gmail.com>
2023-05-16 20:16:22 +0530

    changed navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,8 +30,8 @@ const Navbar = () => {
       className={`${
         styles.paddingX
       } w-full flex items-center py-5 fixed top-0 z-20 ${
-        scrolled ? "bg-primary" : "bg-transparent"
-      }`}
+        scrolled ? "bg-gradient-to-r from-green-500 to-blue-500 animate-glitch" : "bg-transparent"
+      } transition-all duration-500`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -66,14 +66,14 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt="menu"
-            className="w-[28px] h-[28px] object-contain"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
 
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 bg-gradient-to-br from-pink-500 to-purple-500 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl animate-fadeIn`}
           >
             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
@@ -81,7 +81,7 @@ const Navbar = () => {
                   key={nav.id}
                   className={`font-poppins font-medium cursor-pointer text-[16px] ${
                     active === nav.title ? "text-white" : "text-secondary"
-                  }`}
+                  } animate-slideIn`}
                   onClick={() => {
                     setToggle(!toggle);
                     setActive(nav.title);
@@ -91,6 +91,7 @@ const Navbar = () => {
                 </li>
               ))}
             </ul>
+            <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-yellow-500 to-red-500 opacity-50 rounded-b-xl"></div>
           </div>
         </div>
       </div>
